45847782b5c207ddc044dbca665858019b42f1b7
GustavoFreitas22 <gusfmotta@gmail.com>
2023-06-17 12:08:42 -0300

    init project

---
+++ src/main/java/br/com/freitas/dev/delivery_sys/DeliverySysApplication.java
@@ -0,0 +1,13 @@
+package br.com.freitas.dev.delivery_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverySysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/freitas/dev/delivery_sys/DeliverySysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.freitas.dev.delivery_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
